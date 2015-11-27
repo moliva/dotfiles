99dcb2f61eb5ad654eab840493875e369f13e665
moliva <miguel.oliva@mulesoft.com>
2015-11-27 14:13:06 -0300

    Added utils.sh with nofitication function + Refactored git aliases and added a pull for all submodules

--- system/git.sh
+++ system/git.sh
@@ -3,20 +3,22 @@ alias git=hub
 
 alias gpull="git pull --rebase"
 alias gpush="git push origin HEAD"
-alias gstashedpull="git stash; git pull --rebase; git stash pop"
-alias gclone="git clone"
+alias gstashedpull="git stash && git pull --rebase && git stash pop"
+
 alias gcommit="git commit -m"
 alias gaddall="git add --all"
 alias gdiff="git diff"
 alias ghead="cat .git/HEAD" # TODO: would be cool to make this recursively
 alias gammit="gaddall; gcommit"
-alias gapush="gammit $1; gpush"
-alias gfcheckout="git checkout -f"
+alias gapush="gammit $1 &&  gpush"
+
 alias gustash="git stash save --include-untracked"
 alias glstash="git stash list | cat"
 alias gpstash="git stash pop"
-alias glasthash='git log -n 1 --pretty=format:"%H" | ccopy | echo `git log -n 1 --pretty=format:"%H"`'
+
+alias glasthash='git log -n 1 --pretty=format:"%H" && ccopy | echo `git log -n 1 --pretty=format:"%H"`'
 alias glastdiff="git diff HEAD~1 HEAD"
 alias glean="git checkout -- . && git clean -f"
 
+alias gsubpull="git submodule foreach git pull origin HEAD"
 
---
+++ system/utils.sh
@@ -0,0 +1,4 @@
+
+alert() {
+	osascript -e 'display notification "Studio has finished building :)" with title "Built finished!"'
+}
