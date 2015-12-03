99d27a2e0a30278e8f5542fa886a23225fa9edec
moliva <miguel.oliva@mulesoft.com>
2015-12-03 16:27:14 -0300

    Added copy last command function

--- system/utils.sh
+++ system/utils.sh
@@ -2,3 +2,9 @@
 alert() {
 	osascript -e 'display notification "Studio has finished building :)" with title "Built finished!"'
 }
+
+clastcommand() {
+	local lastcommand=$(fc -ln -1)
+        echo $lastcommand | ccopy
+	echo "\"$lastcommand\" copied to clipboard!"
+}
