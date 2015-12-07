004267ddd3779ae09145d334f8512fab41229f44
moliva <miguel.oliva@mulesoft.com>
2015-12-07 16:47:59 -0300

    Added function identifiers to utils.sh

--- system/utils.sh
+++ system/utils.sh
@@ -1,9 +1,9 @@
 
-alert() {
+function alert() {
 	osascript -e 'display notification "Studio has finished building :)" with title "Built finished!"'
 }
 
-clastcommand() {
+function clastcommand() {
 	local lastcommand=$(fc -ln -1)
         echo $lastcommand | ccopy
 	echo "\"$lastcommand\" copied to clipboard!"
