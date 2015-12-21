073689661d382782a048f856bf02c60f41e5c91c
moliva <miguel.oliva@mulesoft.com>
2015-12-21 17:36:33 -0300

    Updated maven script getting rid of all M2 exports (unnecesary for brew version) + added function to easily change between different brew formulas

--- system/maven.sh
+++ system/maven.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env zsh
-# maven related
-
-if [ -z "$M2_HOME" ]; then
-	export M2_HOME=$(brew --prefix maven)/libexec
-fi
 
 export M2_SETTINGS=$HOME/.m2
 export M2_REPO=$M2_SETTINGS/repository
 
-export M2=$M2_HOME/bin
 export MAVEN_OPTS="-Xmx1G -XX:MaxPermSize=512m -Djava.awt.headless=true"
-
-export PATH=$M2:$PATH
 
 alias mvnsettings="edit $M2_SETTINGS/settings.xml"
 
-# TODO: write functions to change easily between brew-managed maven versions
+linkmaven() {
+	local current_maven_formula=$(basename $(dirname $(dirname `mvn --version | sed '/^Maven home: /!d' | sed "s/^Maven home: \(.*\)/\1/"`))) # two dirnames for libexec and version
+	brew unlink $current_maven_formula
+	brew link $1
+}
