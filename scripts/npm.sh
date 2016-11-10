45c66763a42eb8bfd3a4ee343d5cd198f70bad0e
moliva <miguel.oliva@mulesoft.com>
2016-11-10 10:50:51 -0300

    Adding tldr tool to npm deps

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 npm install -g live-server # live reload for websites
 
+npm install -g pug-cli # html dsl cli
+
 npm install -g nodemon # live reload for node apps
 npm install -g forever # runs node apps
 
@@ -11,12 +13,18 @@ npm install -g flightplan # deploy app to host through ssh with rsync
 npm install -g gulp
 npm install -g grunt-cli
 
-npm install -g yo # yeoman
+npm install -g surge # deploy to surge.sh
+
+# project generators
+npm install -g yo
+npm install -g nwb
 
 # yo generators
 # npm install -g webapp-generator # not found
-npm install -g angular-generator 
+# npm install -g angular-generator 
 npm install -g express-generator
-npm install -g generator-zurb-ink # generator for zurb email inliner
+# npm install -g generator-zurb-ink # generator for zurb email inliner
 
 npm install -g js-beautify
+npm install -g tldr
+
