cf909895437ae13df8de5b85998f92097c89d3aa
miguel <miguel.oliva@olx.com>
2018-07-19 13:24:07 -0300

    Creating symbolic link from nvim init to vim rc

--- scripts/vim-install.sh
+++ scripts/vim-install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+mkdir -p ~/.config/nvim
+ln -s "$DOTFILES/vim/vimrc.symlink" "$HOME/.config/nvim/init.vim"
