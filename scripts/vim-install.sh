36d6afff894ccd3467bc69a1cd511f488e1ea5cd
miguel <miguel.oliva@olx.com>
2018-07-19 14:39:44 -0300

    Changing order of commands in vim install script

--- scripts/vim-install.sh
+++ scripts/vim-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-
+# create symbolic link for nvim to use vim profile
 mkdir -p ~/.config/nvim
 ln -s "$DOTFILES/vim/vimrc.symlink" "$HOME/.config/nvim/init.vim"
+
+# install vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
