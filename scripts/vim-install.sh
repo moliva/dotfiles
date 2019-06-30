52fda47aa0f8181197f8a8ad90d4f00f99c32e38
moliva <oliva.miguelh@gmail.com>
2019-06-30 20:32:32 -0300

    Updating some config

--- scripts/vim-install.sh
+++ scripts/vim-install.sh
@@ -5,4 +5,9 @@ ln -s "$DOTFILES/vim/vimrc.symlink" "$HOME/.config/nvim/init.vim"
 
 # install vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+# make nvim point to vim files
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.vim/init.vim
+
 vim +PluginInstall +qall
