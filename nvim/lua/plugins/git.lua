ce4fb549b31a00706f5a407323247e782f4fbfb8
moliva <oliva.miguelh@gmail.com>
2025-01-05 14:27:43 -0300

    chore: add todo

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -19,6 +19,7 @@ return {
     keys = {
       { "<leader>gi", "<cmd>Git init<cr>", desc = "Git Init" },
       { "<leader>gs", vim.cmd.Git, desc = "Git Status" },
+      -- TODO - send push to a separate process - moliva - 2024/12/31
       { "<leader>gp", "<cmd>Git push origin HEAD<cr>", desc = "Git Push" },
       { "<leader>gb", "<cmd>Git blame<cr>", desc = "Git blame" },
       { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git diff" },
