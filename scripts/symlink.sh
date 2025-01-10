3836ef632efd3b88c253b6444e069ac8d26fd29d
Miguel Oliva <miguel@phantom.app>
2025-01-10 18:14:01 -0300

    chore: symlink improvements

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 # from https://github.com/holman/dotfiles
 
-info () {
+info() {
   printf "\r  [ \033[00;34m..\033[0m ] $1\n"
 }
 
-user () {
+user() {
   printf "\r  [ \033[0;33m??\033[0m ] $1\n"
 }
 
-success () {
+success() {
   printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
 
-fail () {
+fail() {
   printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
   echo ''
   exit
@@ -25,14 +25,12 @@ set -e
 
 echo ''
 
-setup_gitconfig () {
-  if ! [ -f "$HOME/.gitconfig.local" ]
-  then
+setup_gitconfig() {
+  if ! [ -f "$HOME/.gitconfig.local" ]; then
     info 'setup gitconfig local'
 
     git_credential='cache'
-    if [ "$(uname -s)" == "Darwin" ]
-    then
+    if [ "$(uname -s)" == "Darwin" ]; then
       git_credential='osxkeychain'
     fi
 
@@ -41,7 +39,7 @@ setup_gitconfig () {
     user ' - What is your github author email?'
     read -e git_authoremail
 
-    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" "git/gitconfig.local.example" > "$HOME/.gitconfig.local"
+    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" "git/gitconfig.local.example" >"$HOME/.gitconfig.local"
 
     success 'gitconfig local'
   else
@@ -49,25 +47,21 @@ setup_gitconfig () {
   fi
 }
 
-
-link_file () {
+link_file() {
   local src=$1 dst=$2
 
   local overwrite= backup= skip=
   local action=
 
-  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
-  then
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]; then
 
-    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
-    then
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]; then
 
       local currentSrc="$(readlink $dst)"
 
-      if [ "$currentSrc" == "$src" ]
-      then
+      if [ "$currentSrc" == "$src" ]; then
 
-        skip=true;
+        skip=true
 
       else
 
@@ -76,20 +70,25 @@ link_file () {
         read -n 1 action
 
         case "$action" in
-          o )
-            overwrite=true;;
-          O )
-            overwrite_all=true;;
-          b )
-            backup=true;;
-          B )
-            backup_all=true;;
-          s )
-            skip=true;;
-          S )
-            skip_all=true;;
-          * )
-            ;;
+        o)
+          overwrite=true
+          ;;
+        O)
+          overwrite_all=true
+          ;;
+        b)
+          backup=true
+          ;;
+        B)
+          backup_all=true
+          ;;
+        s)
+          skip=true
+          ;;
+        S)
+          skip_all=true
+          ;;
+        *) ;;
         esac
 
       fi
@@ -100,38 +99,33 @@ link_file () {
     backup=${backup:-$backup_all}
     skip=${skip:-$skip_all}
 
-    if [ "$overwrite" == "true" ]
-    then
+    if [ "$overwrite" == "true" ]; then
       rm -rf "$dst"
       success "removed $dst"
     fi
 
-    if [ "$backup" == "true" ]
-    then
+    if [ "$backup" == "true" ]; then
       mv "$dst" "${dst}.backup"
       success "moved $dst to ${dst}.backup"
     fi
 
-    if [ "$skip" == "true" ]
-    then
+    if [ "$skip" == "true" ]; then
       info "skipped $src"
     fi
   fi
 
-  if [ "$skip" != "true" ]  # "false" or empty
-  then
+  if [ "$skip" != "true" ]; then # "false" or empty
     ln -s "$1" "$2"
     success "linked $1 to $2"
   fi
 }
 
-create_symlinks () {
+create_symlinks() {
   info 'creating symlinks'
 
   local overwrite_all=false backup_all=false skip_all=false
 
-  for src in $(find -H "$DOTFILES" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
-  do
+  for src in $(find -H "$DOTFILES" -maxdepth 2 -name '*.symlink' -not -path '*.git*'); do
     dst="$HOME/.$(basename "${src%.*}")"
     link_file "$src" "$dst"
   done
@@ -143,6 +137,7 @@ setup_gitconfig
 create_symlinks
 
 # symlink nvim config dir
-link_file "$DOTFILES/nvim" "$HOME/.config/nvim"
+link_file "$DOTFILES/nvim" "$HOME/.config/nvim" || true
 
-link_file "$DOTFILES/starship/starship.toml" "$HOME/.config/starship.toml"
+# symlink starship toml
+link_file "$DOTFILES/starship/starship.toml" "$HOME/.config/starship.toml" || true
